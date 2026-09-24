85fb152189adeda1cbe700e85ff9adbf1134060f
Kris Zhang <kriszhang122507@gmail.com>
2026-09-24 22:15:40 +1200

    Add files via upload

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 // Whiteboard offline support: keeps a copy of the app so it opens without internet.
-const CACHE = 'whiteboard-v5.0.0';
+const CACHE = 'whiteboard-v5.0.1';
 const CORE = ['./', 'index.html', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
